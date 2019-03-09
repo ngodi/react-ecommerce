1125e8bd82245ab08adc739384cacde6b80b1a4b
ngodi <albertngodi@gmail.com>
2019-03-09 23:55:20 +0100

    finalized shopping cart

--- src/context.js
+++ src/context.js
@@ -70,7 +70,17 @@ closeModal = () =>{
 };
 
 increment =(id) =>{
-    console.log("This is the increment method");
+   let tempCart = [...this.state.cart];
+   const selectedProduct = tempCart.find(item => item.id ===id)
+   const index = tempCart.indexOf(selectedProduct);
+   const product = tempCart[index];
+
+   product.count = product.count +1;
+   product.total = product.count* product.price;
+
+   this.setState(()=>{
+       return {cart:[...tempCart]}
+   },()=>{this.addTotals()})
 }
 
 decrement =(id) =>{
@@ -78,14 +88,35 @@ decrement =(id) =>{
 }
 
 removeItem = (id) =>{
-    this.setState(()=>{
-        return {cart:[]}
-    })
-}
+let tempProducts = [...this.state.products];
+let tempCart = [...this.state.cart];
+
+tempCart = tempCart.filter(item => item.id !== id);
+const index = tempProducts.indexOf(this.getItem(id));
+let removedProduct = tempProducts[index];
+removedProduct.inCart = false;
+removedProduct.count = 0;
+removedProduct.total = 0;
+
+this.setState(()=>{
+  return {
+      cart:[...tempCart],
+      products:[...tempProducts]
+  };
+},
+() => {
+    this.addTotals();
+  }
+  );
+};
 
 clearCart = () =>{
-    console.log("cart was cleared");
-}
+   this.setState(()=>{
+       return {cart:[]};
+   },()=>{
+       this.setProducts();
+   });
+};
 
 addTotals =() =>{
  let subTotal = 0;
